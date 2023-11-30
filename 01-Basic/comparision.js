1e206422473107904e213e8c7c4268c14ff2d659
M-mangaraj22 <152247254+M-mangaraj22@users.noreply.github.com>
2023-11-30 05:01:39 +0000

    file added

---
+++ 01-Basic/comparision.js
@@ -0,0 +1,7 @@
+console.log(2>1);
+console.log(2>=81);
+console.log(2<1);
+console.log(2==1);
+console.log(2===1);
+console.log(null>0);
+console.log(null==0);
---
+++ 01-Basic/operation.js
@@ -0,0 +1,19 @@
+//let value =3
+let str1="gift"
+let str2="kbdav"
+let str3=str1+str2;
+//let negvalue = -value
+//console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+let gamecounter=100;
+++gamecounter;
+console.log(gamecounter);
+gamecounter++;
+console.log(gamecounter);
